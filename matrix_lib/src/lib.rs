cf1bbd44ad296b02b36815b088d3c616f7f0af2a
Charles Bennington <c.bennington852@gmail.com>
2025-06-26 19:55:29 -0700

    mores stuff

--- matrix_lib/src/lib.rs
+++ matrix_lib/src/lib.rs
@@ -1,81 +1,126 @@
+//////////////////////////////////////////////////////////////////////
+// For the non-mutable matrix's 
+//////////////////////////////////////////////////////////////////////
 
-/// Returns the min value of the matrix
-pub fn matrix_min(matrix : &[&[i32]]) -> i32 {
-    let mut curr_min = matrix[0][0];
-    for row in matrix {
-        for val in *row {
-            if *val < curr_min {
-                curr_min = *val
+trait Matrix {
+    fn matrix_min(self) -> i32;
+    fn matrix_max(self) -> i32;
+    fn matrix_median(self) -> i32;
+    fn matrix_print(self);
+    fn matrix_avg(self) -> f64;
+    fn matrix_scale(self , scalar : i32);
+}
+
+impl Matrix for &[&[i32]] {
+    /// Returns the min value of the matrix
+    fn matrix_min(self) -> i32 {
+        let mut curr_min = self[0][0];
+        for row in self {
+            for val in *row {
+                if *val < curr_min {
+                    curr_min = *val
+                }
+            }
+        }
+        return curr_min;
+    }
+
+    /// Returns the max value of the matrix
+    fn matrix_max(self) -> i32 {
+        let mut curr_max = self[0][0];
+        for row in self {
+            for val in *row {
+                if *val > curr_max {
+                    curr_max = *val
+                }
+            }
+        }
+        return curr_max;
+    }
+
+    ///returns the averge of the matrix
+    fn matrix_avg(self) -> f64 {
+        let mut sum = 0;
+        let mut size = 0;
+        for row in self {
+            for val in *row {
+                sum += *val;
+                size += 1;
+            }
+        }
+        return sum as f64 / size as f64;
+    }
+
+    ///returns the sum of the matrix
+    fn matrix_sum(self) -> i32{
+        let mut sum : i32 = 0;
+        for row in self {
+            for val in *row {
+                sum += *val;
+            }
+        }
+        return sum;
+    }
+
+    //fn matrix_median(matrix)
+    fn matrix_median(self) -> i32 {
+        //flatten the matrix
+        let mut flat : Vec<i32> = Vec::new();
+        for row in self {
+            for val in *row {
+                flat.push(*val);
+            }
+        }
+
+        //sort the flattened matrix
+        flat.sort();
+
+        //return the median
+        let result = flat.get(flat.len() / 2);
+        match result {
+            Some(result) => return *result,
+            None => panic!("Unexpected error"),
+        }
+    }
+
+    ///prints the matrix
+    fn matrix_print(self) {
+        for row in self {
+            print!("|");
+            for val in *row {
+                print!(" {} ",*val)
+            }
+            println!("|");
+        }
+    }
+
+    ///returns a new scaled matrix
+    fn matrix_scale(self , scalar : i32) {
+
+    }
+}
+
+/////////////////////////////////////////////////////////////////////
+// Mutable Matrix's 
+/////////////////////////////////////////////////////////////////////
+
+impl Matrix for &mut[&mut[i32]] {
+    //applies a scalar to the matrix
+    fn matrix_scale(self , scalar : i32) {
+        for row in self {
+            for val in &mut **row {
+                *val = *val * scalar
             }
         }
     }
-    return curr_min;
-}
 
-/// Returns the max value of the matrix
-pub fn matrix_max(matrix : &[&[i32]]) -> i32 {
-    let mut curr_max = matrix[0][0];
-    for row in matrix {
-        for val in *row {
-            if *val > curr_max {
-                curr_max = *val
+    fn matrix_print(self) {
+        for row in self {
+            print!("|");
+            for val in &mut **row {
+                print!(" {} ",*val)
             }
-        }
-    }
-    return curr_max;
-}
-
-///returns the averge of the matrix
-pub fn matrix_avg(matrix : &[&[i32]]) -> f64 {
-    let mut sum = 0;
-    let mut size = 0;
-    for row in matrix {
-        for val in *row {
-            sum += *val;
-            size += 1;
-        }
-    }
-    return sum as f64 / size as f64;
-}
-
-///returns the sum of the matrix
-pub fn matrix_sum(matrix: &[&[i32]]) -> i32{
-    let mut sum : i32 = 0;
-    for row in matrix {
-        for val in *row {
-            sum += *val;
-        }
-    }
-    return sum;
-}
-
-//pub fn matrix_median(matrix)
-pub fn matrix_median(matrix: &[&[i32]]) -> i32 {
-    //flatten the matrix
-    let mut flat : Vec<i32> = Vec::new();
-    for row in matrix {
-        for val in *row {
-            flat.push(*val);
-        }
-    }
-
-    //sort the flattened matrix
-    flat.sort();
-
-    //return the median
-    let result = flat.get(flat.len() / 2);
-    match result {
-        Some(result) => return *result,
-        None => panic!("Unexpected error"),
-    }
-}
-
-//applies a scalar to the matrix
-pub fn matrix_scale(matrix: &mut[&mut[i32]] , scalar : i32) {
-    for row in matrix {
-        for val in &mut **row {
-            *val = *val * scalar
+            println!("|");
         }
     }
 }
-
--- matrix_lib/tests/test.rs
+++ matrix_lib/tests/test.rs
@@ -53,15 +53,24 @@ mod tests {
 
         let matrix: &[&[i32]] = &[&row1, &row2, &row3];
         assert_eq!(5 , matrix_median(matrix));
+        matrix_print(matrix);
     }
 
     #[test]
     fn scalar_test() {
-        let row1 = [5, 2, 3];
-        let row2 = [4, 5, 6];
-        let row3 = [7, 8, 9];
+        let mut row1 = [2, 2, 2];
+        let mut row2 = [3, 3, 3];
+        let mut row3 = [5, 5, 5];
 
-        let matrix: &[&[i32]] = &[&row1, &row2, &row3];
-        assert_eq!(5 , matrix_median(matrix));
+        let matrix: &mut[&mut[i32]] = &mut[&mut row1, &mut row2, &mut row3];
+
+
+        let mut res1 = [4, 4, 4];
+        let mut res2 = [6, 6, 6];
+        let mut res3 = [10, 10, 10];
+
+        let res: &mut[&mut[i32]] = &mut[&mut res1, &mut res2, &mut res3];
+
+        assert_eq!(1 , 1);
     }
 }
